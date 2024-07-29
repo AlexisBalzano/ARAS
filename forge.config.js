0429049c4d4acc6f525fb68348c34d55850445c2
Alexis <alexis.balzano@epfl.ch>
2024-07-29 17:37:54 +0200

    autoupdate initial testing

---
+++ forge.config.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+module.exports = {
+  packagerConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+  ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "github-user-name",
+          name: "github-repo-name",
+        },
+        authToken: process.env.GITHUB_AUTH_TOKEN,
+        prerelease: false,
+        draft: true,
+        generateReleaseNotes: true,
+      },
+    },
+  ],
+};
